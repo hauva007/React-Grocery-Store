92590df038d0d6859057f318a3679fcb1b0450f0
Jarno Lahtinen <lahtinen.jarno@gmail.com>
2017-12-08 21:48:24 +0200

    Making app isomorphic. Added babel and React server side rendering

--- server/main.js
+++ server/main.js
@@ -1,13 +1,21 @@
 let express = require('express');
-
 let app = new express();
-
 let parser = require('body-parser');
+let React = require('react');
+let ReactDOMServer = require('react-dom/server');
+let GroceryItem = require('./models/GroceryItem.js')
 
+require('babel-register');
 require('./database.js');
 
 app.get('/', function(req, res) {
-    res.render('./../app/index.ejs',{})    
+    let application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
+    GroceryItem.find(function(error, doc) {
+        let generated = ReactDOMServer.renderToString(application({
+            items: doc
+        }));
+        res.render('./../app/index.ejs', { reactOutput: generated });
+    });
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
